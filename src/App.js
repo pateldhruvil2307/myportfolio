41edb8db96deb6720443ce5f4cf989a6606ba8f4
patel dhruvil <pateldhruvilk2@gmail.com>
2023-09-20 22:51:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React  from 'react';
 import { useState } from 'react';
 import './App.css';
-import { BrowserRouter,Route,Router, Routes } from 'react-router-dom';
-
 import Home from './componets/Home';
 
 function App(){
@@ -10,16 +8,7 @@ function App(){
 return(
   <>
  <Home/>
-<Routes>
-<Route path='/Home' element={Home} />
 
-
-</Routes>
-
- 
-
-
-  
   </>
 )
 }
